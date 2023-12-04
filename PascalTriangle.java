3cc33c37c72abf004707f8f5f01c09cc35a4dd9c
Chandana Bhagwat <93093759+chandana011@users.noreply.github.com>
2023-12-04 17:05:56 +0530

    Update PascalTriangle.java

--- PascalTriangle.java
+++ PascalTriangle.java
@@ -1,3 +1,13 @@
+//Easy--example3
+//Question--->
+//Given an integer numRows, return the first numRows of Pascal's triangle.
+//In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+//    1
+//   1 1
+//  1 2 1
+// 1 3 3 1
+//1 4 6 4 1
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
