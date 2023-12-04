8ce37291c49deba17270d475aca679ba9670bba3
Chandana Bhagwat <93093759+chandana011@users.noreply.github.com>
2023-12-04 17:00:33 +0530

    Update LengthOfLastWord.java

--- LengthOfLastWord.java
+++ LengthOfLastWord.java
@@ -1,5 +1,6 @@
 //Easy -----> Example 1
-
+////Question---->
+//Given a string s consisting of words and spaces, return the length of the last word in the string.A word is a maximal substring consisting of non-space characters only.
 
 import java.util.Scanner;
 public class LengthOfLastWord {
